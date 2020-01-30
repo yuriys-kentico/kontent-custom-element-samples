8bc60f79ed549b77ddbcdbf72060b873f00dddef
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-01-30 09:04:36 -0500

    Update Summernote repo link

--- gallery/sw.js
+++ gallery/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ad4275018f6d53583113.js"
+    "url": "webpack-runtime-52f268dd6db260c098b0.js"
   },
   {
     "url": "styles.b482f24de80c64c61587.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f8f8d6e8ce4830f378f57720a9029577"
+    "revision": "ad8afacba922f830bd216b4aae69248c"
   },
   {
     "url": "component---src-pages-404-js-aa8025e0cb63ac64d9fe.js"
