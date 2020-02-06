aeac1d5b9854a3e2a6b68b2cfca0b882fba82119
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-02-06 11:47:02 -0500

    Update giphy selector

--- gallery/sw.js
+++ gallery/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6ede4577dc81aa8cda4d.js"
+    "url": "webpack-runtime-b72bd40d1542349cc9ca.js"
   },
   {
     "url": "styles.b482f24de80c64c61587.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7645ec3ec2d799099acb551f85552965"
+    "revision": "988706d9b2259f0f37dd95d498ca7ebd"
   },
   {
     "url": "component---src-pages-404-js-aa8025e0cb63ac64d9fe.js"
