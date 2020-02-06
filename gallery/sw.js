4386c1369f8d571e20dcaec651b9b6a4be7e56e5
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-02-06 11:02:48 -0500

    rename flickr image selector

--- gallery/sw.js
+++ gallery/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-06a6e1d2828b80b12aec.js"
+    "url": "webpack-runtime-6ede4577dc81aa8cda4d.js"
   },
   {
     "url": "styles.b482f24de80c64c61587.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a57ab03d03eb46892c2d3abbf532243c"
+    "revision": "7645ec3ec2d799099acb551f85552965"
   },
   {
     "url": "component---src-pages-404-js-aa8025e0cb63ac64d9fe.js"
