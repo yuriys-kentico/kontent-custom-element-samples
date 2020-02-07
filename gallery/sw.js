9e71da289d7eff35a440942c49f3401dd4f451b2
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2020-02-07 12:04:54 -0500

    Update build with repo URL changes

--- gallery/sw.js
+++ gallery/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-eda239774f25ce809f24.js"
+    "url": "webpack-runtime-45e7564201caa65e8b92.js"
   },
   {
     "url": "styles.b482f24de80c64c61587.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "84899c94cb6a67f41211f956deb32cd1"
+    "revision": "273cab1cfc430898a4079d4f34e3e468"
   },
   {
     "url": "component---src-pages-404-js-aa8025e0cb63ac64d9fe.js"
