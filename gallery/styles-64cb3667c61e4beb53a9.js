0220051b2d2ceb18af6c73ce96263bb424b40a01
ondrejch <ondrejch@kentico.com>
2020-07-17 17:38:33 +0200

    remove gallery folder

--- gallery/styles-64cb3667c61e4beb53a9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{DgS1:function(n,w,o){},SRwX:function(n,w,o){},ZHwO:function(n,w,o){}}]);
-//# sourceMappingURL=styles-64cb3667c61e4beb53a9.js.map
--- gallery/styles-8bc6f6dbadd199c10625.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{SRwX:function(n,w,o){},ZHwO:function(n,w,o){}}]);
-//# sourceMappingURL=styles-8bc6f6dbadd199c10625.js.map
